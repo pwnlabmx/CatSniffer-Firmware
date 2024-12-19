2c39f808d9f95cd30b7c83aee225d84c4d561e8a
Marcelo Arredondo <marcelo52_mad@hotmail.com>
2024-12-19 15:57:07 -0600

    fix:  device name automation and function de finitions

--- RP2040/advertise/advertise.ino
+++ RP2040/advertise/advertise.ino
@@ -14,54 +14,56 @@ uint8_t LEDs[3]={LED1,LED2,LED3};
 int i=0;
 
 // Define advertisement data, hardcoded for now
+
 uint8_t advData[] = {
-  0x02, 0x01, 0x1A, 0x02, 0x0A, 0x0C, 0x11, 0x07,
+  0x03, 0x01, 0x1A, 0x02, 0x0A, 0x0C, 0x11, 0x07,
   0x64, 0x14, 0xEA, 0xD7, 0x2F, 0xDB, 0xA3, 0xB0,
   0x59, 0x48, 0x16, 0xD4, 0x30, 0x82, 0xCB, 0x27,
   0x05, 0x03, 0x0A, 0x18, 0x0D, 0x18
 };
-uint8_t advDataLen = sizeof(advData) / sizeof(advData[0]);
+
+/*
+uint8_t advData[] = {
+  0x1E, 0xFF, 0x4C, 0x00, 0x12, 0x19, 0x10, 0x62,
+  0x99, 0x96, 0x85, 0xAB, 0x4C, 0x0A, 0xB4, 0xA1,
+  0xD6, 0xA4, 0x13, 0xA1, 0x9B, 0x30, 0x84, 0x4D,
+  0x60, 0x70, 0x59, 0xCF, 0xA2, 0x01, 0x45
+};
+*/
 
 //Define the scan response data, hardcoded for now 
 String devName = "CatSniffer"; //0b 09 43 61 74 53 6e 69 66 66 65 72 Define the name of the device
-/*
-int devNameLen = strlen(devName);//
-uint8_t scanRspData[devNameLen +2]; //
-uint8_t scanRspData[0] = devNameLen;
-uint8_t scanRspData[1] = 9;
-for (int i = 0; i < (devNameLen + 2); i++) { //Copy each value of advData to paddedAdvData starting from second value
-  scanRspData[i+2] = devName[i];
-}
-*/
-uint8_t scanRspData[] = {0x0B, 0x09, 0x43, 0x61, 0x74, 0x53, 0x6E, 0x69, 0x66, 0x66, 0x65, 0x72}; // Name hard coded, lenght of name + 0x09 + CatSniffer in Hex
-
-uint8_t scanRspDataLen = sizeof(scanRspData) / sizeof(scanRspData[0]);
+//uint8_t scanRspData[] = {0x0B, 0x09, 0x43, 0x61, 0x74, 0x53, 0x6E, 0x69, 0x66, 0x66, 0x65, 0x72}; // Name hard coded, lenght of name + 0x09 + CatSniffer in Hex
 
 
 //Function definitions
-uint8_t cmdAdvertise(uint8_t *advData, uint8_t *scanRspData, uint8_t mode);
-void cmdSend(int mode, byte* paddedAdvData, byte* paddedScanRspData);
+int8_t cmdAdvertise(uint8_t * advData, uint8_t advDataLen, uint8_t * scanRspData, uint8_t scanRspDataLen, uint8_t mode);
 
+void cmdSend(int mode, uint8_t* paddedAdvData, uint8_t* paddedScanRspData);
 
-void setup(){
-  uint8_t devNameLen = devName.length();
-  uint8_t scanRspData_1[devNameLen +2]; 
-  scanRspData_1[0] = devNameLen;
-  scanRspData_1[1] = 0x09;
+void setup(){   
+
+  uint8_t advDataLen = sizeof(advData) / sizeof(advData[0]);
+
+  uint8_t devNameLen = devName.length() + 1;
+  uint8_t scanRspData[devNameLen +2]; 
+  scanRspData[0] = devNameLen;
+  scanRspData[1] = 0x09;
   for (int i = 0; i < devNameLen; i++) { //Copy each value of advData to paddedAdvData starting from second value
-    scanRspData_1[i+2] = devName[i];
+    scanRspData[i+2] = devName[i];
   }
-    
-    
+
+  uint8_t scanRspDataLen = sizeof(scanRspData) / sizeof(scanRspData[0]);
+
+
     catsniffer.led_interval=1000;
     catsniffer.baud=921600;
     catsniffer.mode=PASSTRHOUGH;  
     
     Serial.begin(catsniffer.baud);
     Serial1.begin(catsniffer.baud);
-    
+
     while (!Serial) ;
-    
     
     pinMode(Pin_Button, INPUT_PULLUP);
     pinMode(Pin_Boot, INPUT_PULLUP);
@@ -110,21 +112,9 @@ void setup(){
       Serial.print(" ");
     }
       Serial.print("\n");
-
-    Serial.println("Scan Response Data 1 is:");
-    for(int i = 0; i < scanRspDataLen; i++)
-    {
-      if(scanRspData_1[i]>0x0F)
-        Serial.print("0x");
-      else
-        Serial.print("0x0");
-      Serial.print(scanRspData_1[i],HEX); //Add , HEX again if needed
-      Serial.print(" ");
-    }
-      Serial.print("\n");
 #endif
     
-    uint8_t error=cmdAdvertise(advData,scanRspData,0); /////////////////////////////////////////////////////////////////// MODE ////////////////////////////////////////////////////////////////////////
+    int8_t error = cmdAdvertise(advData, advDataLen, scanRspData, scanRspDataLen, 0); /////////////////////////////////////////////////////////////////// MODE ////////////////////////////////////////////////////////////////////////
     //Error handling
     if (error == -1)
     {
@@ -161,7 +151,7 @@ void loop() {
 
 
 
-uint8_t cmdAdvertise(uint8_t *advData, uint8_t *scanRspData, uint8_t mode) {
+int8_t cmdAdvertise(uint8_t * advData, uint8_t advDataLen, uint8_t * scanRspData, uint8_t scanRspDataLen, uint8_t mode) {
     if (advDataLen > 31) {
         return -1;// Error: advData too long
     }
@@ -173,11 +163,13 @@ uint8_t cmdAdvertise(uint8_t *advData, uint8_t *scanRspData, uint8_t mode) {
     if (mode != 0 && mode != 2 && mode != 3) {
         return -3;// Error: Mode must be 0 (connectable), 2 (non-connectable), or 3 (scannable)
     }
+  
     
 #ifdef TEST
-    Serial.println("advDataLen: ");
+    Serial.print("advDataLen: ");
     Serial.println(advDataLen);
-    Serial.println("advDataLen: ");
+
+    Serial.print("scanRspDataLen: ");
     Serial.println(scanRspDataLen);
 #endif
 
@@ -185,19 +177,26 @@ uint8_t cmdAdvertise(uint8_t *advData, uint8_t *scanRspData, uint8_t mode) {
     uint8_t paddedScanRspData[32]={0}; //Creat Padded Scan Response Data Array
 
     //Pass data from one array to another
-    paddedAdvData[0] = (uint8_t)advDataLen; //make first value of paddedAdvData advDataLen
-    for (int i = 0; i < advDataLen + 1; i++) { //Copy each value of advData to paddedAdvData starting from second value
-      paddedAdvData[i+1] = advData[i];
+
+    //make first value of paddedAdvData advDataLen
+    paddedAdvData[0] = advDataLen; 
+    //Copy each value of advData to paddedAdvData starting from second value
+    for (int i = 0; i < advDataLen; i++) {
+      paddedAdvData[i +1] = advData[i];
     }
-    for (int i = advDataLen +1; i < 32; i++) { //Fill with 0s
+    //Fill with 0s
+    for (int i = advDataLen +1; i < 32; i++) { 
       paddedAdvData[i] = 0;
     }
 
-    paddedScanRspData[0] = (uint8_t)scanRspDataLen; //make first value of paddedScanRspData c
-    for (int i = 0; i < scanRspDataLen + 1; i++) { //Copy each value of scanRspData to paddedScanRspData starting from second value
-      paddedScanRspData[i+1] = scanRspData[i];
+    //make first value of paddedScanRspData 
+    paddedScanRspData[0] = scanRspDataLen; 
+    //Copy each value of scanRspData to paddedScanRspData starting from second value
+    for (int i = 0; i < scanRspDataLen; i++) { 
+      paddedScanRspData[i + 1] = scanRspData[i];
     }
-    for (int i = scanRspDataLen +1; i < 32; i++) { //Fill with 0s
+    //Fill with 0s
+    for (int i = scanRspDataLen +1; i < 32; i++) { 
       paddedScanRspData[i] = 0;
     }
 
@@ -235,9 +234,9 @@ uint8_t cmdAdvertise(uint8_t *advData, uint8_t *scanRspData, uint8_t mode) {
 }
 
 
-void cmdSend(int mode, byte* paddedAdvData, byte* paddedScanRspData) {
+void cmdSend(int mode, uint8_t * paddedAdvData, uint8_t * paddedScanRspData) {
     // Create a byte array to hold the command
-    byte cmdByteList[66]; //[1 + 1 + 32 + 32]
+    uint8_t cmdByteList[66]; //[1 + 1 + 32 + 32]
     
     // Assign b0 to the first byte of cmd
     cmdByteList[0] = 0X1C;
@@ -368,8 +367,8 @@ unsigned long startTime = millis();
 while (millis() - startTime < duration) {
   if (Serial1.available()) {
     int incomingByte = Serial1.read(); //read serial and save it to a variable
-    Serial.print("Byte original: ");
-    Serial.println(incomingByte, HEX);
+    //Serial.print("Byte original: ");
+    //Serial.println(incomingByte, HEX);
     //char incomingByteArray[100];
     //itoa(incomingByte, incomingByteArray, 10);
 
