a0118f5fdf4877d4bc0e9f64ebca8243f68b65ac
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2025-01-24 17:21:48 -0600

    feat(lora-freq): Add freq start & end cmd

--- RP2040/LoRa-Freq/LoRa-Freq.ino
+++ RP2040/LoRa-Freq/LoRa-Freq.ino
@@ -9,6 +9,9 @@
   please buy us a round!
   Distributed as-is; no warranty is given.
 */
+
+#define SERIALCOMMAND_HARDWAREONLY
+#include <SerialCommand.h> // https://github.com/kroimon/Arduino-SerialCommand
 #include <RadioLib.h>
 
 // this file contains binary patch for the SX1262
@@ -18,9 +21,18 @@
 #define CTF2 9
 #define CTF3 10
 
+#define FREQ_RANGE_START 860
+#define FREQ_RANGE_END 928
+
 #define LED1 (27)
 #define LED2 (26)
 #define LED3 (28)
+
+struct FreqContext{
+  float freqStart;
+  float freqEnd;
+  float freq;
+};
 
 uint8_t LEDs[3]={LED1,LED2,LED3};
 
@@ -31,17 +43,26 @@ uint8_t LEDs[3]={LED1,LED2,LED3};
 // BUSY pin:  4
 SX1262 radio = new Module(17, 5, 24, 4);
 
-// frequency range in MHz to scan
-const float freqStart = 868;
-const float freqEnd = 928;
+SerialCommand SCmd;
+
+FreqContext freqCtx;
 
 void setup() {
   Serial.begin(921600);
   while(!Serial)
 
+  // Callbacks for serial commands
+  SCmd.addCommand("set_freq_start", cmdSetFreqStart);
+  SCmd.addCommand("set_freq_end", cmdSetFreqEnd);
+  SCmd.setDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "What?")
+
+  // frequency range in MHz to scan
+  freqCtx.freqStart = FREQ_RANGE_START;
+  freqCtx.freqEnd = FREQ_RANGE_END;
+  
   // initialize SX1262 FSK modem at the initial frequency
   Serial.println(F("DONE: Initializing ... "));
-  int state = radio.beginFSK(freqStart,4.8,5.0,156.2,10,16,0,false);
+  int state = radio.beginFSK(freqCtx.freqStart,4.8,5.0,156.2,10,16,0,false);
   if(state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -88,10 +109,46 @@ void setup() {
   digitalWrite(LED3, 0);
 }
 
+
+void cmdSetFreqStart(){
+  char *arg;
+  arg = SCmd.next();
+  float tmpFreq = atoi(arg);
+  if (arg != NULL){
+    
+    if(tmpFreq > freqCtx.freqEnd){
+      Serial.println(F("Selected frequency is invalid for this module!"));
+      return;
+    }
+    freqCtx.freqStart = tmpFreq;
+    radio.setFrequency(tmpFreq);
+  }else{
+    Serial.println(F("Invalid argument!"));
+  }
+}
+
+void cmdSetFreqEnd(){
+  char *arg;
+  arg = SCmd.next();
+  float tmpFreq = atoi(arg);
+  if (arg != NULL){
+    
+    if(tmpFreq < freqCtx.freqStart){
+      Serial.println(F("Selected frequency is invalid for this module!"));
+      return;
+    }
+    freqCtx.freqEnd = tmpFreq;
+    radio.setFrequency(tmpFreq);
+  }else{
+    Serial.println(F("Invalid argument!"));
+  }
+}
+
 void loop() {
+  SCmd.readSerial();     // We don't do much, just process serial commands
   // perform scan over the entire frequency range
-  float freq = freqStart;
-  while(freq <= freqEnd) {
+  freqCtx.freq = freqCtx.freqStart;
+  while(freqCtx.freq <= freqCtx.freqEnd) {
     // Serial.print("FREQ ");
     // Serial.println(freq, 2);
 
@@ -120,7 +177,7 @@ void loop() {
       digitalWrite(LED2, 1);
       digitalWrite(LED3, 1);
       Serial.print("@S");
-      Serial.print(freq, 2);
+      Serial.print(freqCtx.freq, 2);
       Serial.print(";");
       for(uint8_t i = 0; i < RADIOLIB_SX126X_SPECTRAL_SCAN_RES_SIZE; i++) {
         Serial.print(results[i]);
@@ -135,12 +192,16 @@ void loop() {
     // set the next frequency
     // the frequency step should be slightly smaller
     // or the same as the Rx bandwidth set in setup
-    freq += 0.2;
-    radio.setFrequency(freq);
+    freqCtx.freq += 0.2;
+    radio.setFrequency(freqCtx.freq);
     digitalWrite(LED1, 0);
     digitalWrite(LED2, 0);
     digitalWrite(LED3, 0);
   }
 
   
+}
+
+void unrecognized(const char *command) {
+  Serial.println("Command not found, type help to get the valid commands");
 }
