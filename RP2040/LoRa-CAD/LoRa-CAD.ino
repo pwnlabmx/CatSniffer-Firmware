b33c4402093b8ddc80f532e179dda78e3f587306
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2025-01-27 14:28:23 -0600

    feat(CAD): Add main commands

--- RP2040/LoRa-CAD/LoRa-CAD.ino
+++ RP2040/LoRa-CAD/LoRa-CAD.ino
@@ -10,6 +10,9 @@
   Distributed as-is; no warranty is given.
 */
 
+#define SERIALCOMMAND_HARDWAREONLY
+
+#include <SerialCommand.h>
 #include <RadioLib.h>
 
 #define CTF1 8
@@ -28,9 +31,23 @@ uint8_t LEDs[3] = { LED1, LED2, LED3 };
 // NRST pin:  24
 // BUSY pin:  4
 SX1262 radio = new Module(17, 5, 24, 4);
+SerialCommand SCmd;
+
+struct RadioContext {
+  float frequency;
+  int bandWidth;
+  int spreadFactor;
+  int codingRate;
+  byte syncWord;
+  int preambleLength;
+  int outputPower;
+};
+
+RadioContext radioCtx;
 
 // whether we are receiving, or scanning
 bool receiving = false;
+bool runningScan = false;
 // flag to indicate that a packet was successfully printed
 bool recivedPacket = false;
 const unsigned long interval = 5000;    // 5 s interval to send message
@@ -63,9 +80,34 @@ void setup() {
   digitalWrite(LED1, 0);
   digitalWrite(LED2, 0);
   digitalWrite(LED3, 0);
+
+  SCmd.addCommand("set_freq", setFrequency);
+  SCmd.addCommand("set_sf", setSpreadFactor);
+  SCmd.addCommand("set_bw", setBandWidth);
+  SCmd.addCommand("set_cr", setCodingRate);
+  SCmd.addCommand("set_sw", setSyncWord);
+  SCmd.addCommand("set_pl", setPreambleLength);
+  SCmd.addCommand("set_op", setOutputPower);
+  SCmd.addCommand("start", setScanningStart);
+  SCmd.addCommand("stop", setScanningStop);
+  
+  SCmd.addCommand("get_config", getConfiguration);
+  SCmd.addCommand("get_state", getScanning);
+  SCmd.addCommand("help", help);
+  
+
+  SCmd.setDefaultHandler(unrecognized);
   // initialize SX1262 with default settings
+  radioCtx.frequency = 903.9;
+  radioCtx.bandWidth = 250;
+  radioCtx.spreadFactor = 7;
+  radioCtx.codingRate = 5;
+  radioCtx.syncWord = 0x34;
+  radioCtx.outputPower = 20;
+  radioCtx.preambleLength = 10;
+  
   Serial.print(F("[SX1262] Initializing ... "));
-  int state = radio.begin(903.9, 250, 7, 5, 0x34, 20, 10, 0, false);
+  int state = radio.begin(radioCtx.frequency, radioCtx.bandWidth, radioCtx.spreadFactor, radioCtx.codingRate, radioCtx.syncWord, radioCtx.outputPower, radioCtx.preambleLength, 0, false);
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -81,19 +123,11 @@ void setup() {
   // when LoRa packet or timeout is detected
   radio.setDio1Action(setFlag);
 
-  // start scanning the channel
-  Serial.print(F("[SX1262] Starting scan for LoRa preamble ... "));
-  state = radio.startChannelScan();
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("success!"));
-  } else {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-  }
+  startRadioScanning();
 }
 
 void resetScan(){
-  if (!receiving) {
+  if (!receiving && runningScan) {
     Serial.print(F("[SX1262] Starting scan for LoRa preamble ... "));
     int state = radio.startChannelScan();
     if (state == RADIOLIB_ERR_NONE) {
@@ -106,8 +140,9 @@ void resetScan(){
 }
 
 void loop() {
+  SCmd.readSerial();
   // check if the flag is set
-  if (scanFlag) {
+  if (scanFlag && runningScan) {
     int state = RADIOLIB_ERR_NONE;
 
     // reset flag
@@ -198,4 +233,198 @@ void loop() {
       }
     }
   }
+}
+
+void startRadioScanning(){
+  // start scanning the channel
+  Serial.print(F("[SX1262] Starting scan for LoRa preamble ... "));
+  int state = radio.startChannelScan();
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+    runningScan = false;
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+}
+
+void setPreambleLength(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    int tmp_value = atoi(arg);
+    if(radio.setPreambleLength(tmp_value) == RADIOLIB_ERR_INVALID_PREAMBLE_LENGTH){
+      Serial.println(F("Selected preamble length is invalid for this module!"));
+      return;
+    }
+    Serial.println("Preamble Length set to " + String(tmp_value));
+    radioCtx.preambleLength = tmp_value;
+  }
+}
+
+void setSyncWord(){
+  char *arg;
+  byte syncWord;
+  arg = SCmd.next();
+  if(arg != NULL){
+    if ((arg[0] > 64 && arg[0] < 71 || arg[0] > 47 && arg[0] < 58) && (arg[1] > 64 && arg[1] < 71 || arg[1] > 47 && arg[1] < 58) && arg[2] == 0){
+      syncWord = 0;
+      syncWord = nibble(*(arg)) << 4;
+      syncWord = syncWord | nibble(*(arg + 1));
+      if (radio.setSyncWord(syncWord) != RADIOLIB_ERR_NONE) {
+        Serial.println(F("Unable to set sync word!"));
+        return;
+      }
+      Serial.print("Sync word set to 0x");
+      Serial.println(syncWord, HEX);
+      radioCtx.syncWord = syncWord;
+    }else{
+      Serial.println("Use yy value. The value yy represents any pair of hexadecimal digits. ");
+      return;
+    }
+  }
+}
+
+void setOutputPower(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    int tmp_value = atoi(arg);
+    if(radio.setOutputPower(tmp_value) == RADIOLIB_ERR_INVALID_CODING_RATE){
+      Serial.println(F("Selected output power is invalid for this module!"));
+      return;
+    }
+    Serial.println("Output Power set to " + String(tmp_value));
+    radioCtx.outputPower = tmp_value;
+  }
+}
+
+void setCodingRate(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    int tmp_value = atoi(arg);
+    if(radio.setSpreadingFactor(tmp_value) == RADIOLIB_ERR_INVALID_CODING_RATE){
+      Serial.println(F("Selected coding rate is invalid for this module!"));
+      return;
+    }
+    Serial.println("Coding Rate set to " + String(tmp_value));
+    radioCtx.codingRate = tmp_value;
+  }
+}
+
+void setSpreadFactor(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    int tmp_value = atoi(arg);
+    if(radio.setSpreadingFactor(tmp_value) == RADIOLIB_ERR_INVALID_SPREADING_FACTOR){
+      Serial.println(F("Selected spread factor is invalid for this module!"));
+      return;
+    }
+    Serial.println("Spreadfactor set to " + String(tmp_value));
+    radioCtx.spreadFactor = tmp_value;
+  }
+}
+
+void setBandWidth(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    float tmp_value = atoi(arg);
+    if(radio.setBandwidth(tmp_value) == RADIOLIB_ERR_INVALID_BANDWIDTH){
+      Serial.println(F("Selected bandwidth is invalid for this module!"));
+      return;
+    }
+    Serial.println("Bandwidth set to " + String(tmp_value) + " kHz");
+    radioCtx.bandWidth = tmp_value;
+  }
+}
+
+void setFrequency(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    float tmp_value = atoi(arg);
+    if(radio.setFrequency(tmp_value) == RADIOLIB_ERR_INVALID_FREQUENCY){
+      Serial.println(F("Selected frequency is invalid for this module!"));
+      return;
+    }
+    Serial.println("Frequency set to " + String(tmp_value) + " MHz");
+    radioCtx.frequency = tmp_value;
+  }
+}
+
+void getConfiguration(){
+  Serial.println("Radio Configuration");
+  Serial.println("Frequency = " + String(radioCtx.frequency) + " MHz");
+  Serial.println("Bandwidth = " + String(radioCtx.bandWidth));
+  Serial.println("Spreading Factor = " + String(radioCtx.spreadFactor));
+  Serial.println("Coding Rate = 4/" + String(radioCtx.codingRate));
+  Serial.print("Sync Word = 0x");
+  Serial.println(radioCtx.syncWord, HEX);
+  Serial.println("Preamble Length = " + String(radioCtx.preambleLength));
+  Serial.println("Output Power = " + String(radioCtx.outputPower));
+}
+
+void setScanningStart(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    if(!runningScan){
+      startRadioScanning();
+    }
+  }
+}
+
+void setScanningStop(){
+  char *arg;
+  arg = SCmd.next();
+  if(arg != NULL){
+    if(runningScan){
+      scanFlag = false;
+      runningScan = false;
+      receiving = false;
+    }
+  }
+}
+
+void getScanning(){}
+
+void help(){
+  Serial.println("Available commands are:");
+  Serial.print("set_freq");
+  Serial.println("Set the frequency in range of 150/960 MHz: Default 903.9");
+  Serial.print("set_sf");
+  Serial.println("Set the spread factor. Default: 7");
+  Serial.print("set_bw");
+  Serial.println("Set the bandwith value. Options: (7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250, 500) kHz: Default 250");
+  Serial.print("set_cr");
+  Serial.println("Set the coding rate. Default: 5");
+  Serial.print("set_sw");
+  Serial.println("Set the sync Word: Default: 0x34");
+  Serial.print("set_pl");
+  Serial.println("Set the preamble length: Default: 10");
+  Serial.print("set_op");
+  Serial.println("Set the output power. Default: 20");
+  Serial.println("get_config Show the configuration of the radio");
+}
+
+void unrecognized(const char *command) {
+  Serial.println("Command not found, type help to get the valid commands");
+}
+
+
+byte nibble(char c)
+{
+  if (c >= '0' && c <= '9')
+    return c - '0';
+
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+
+  return 0;  // Not a valid hexadecimal character
 }
